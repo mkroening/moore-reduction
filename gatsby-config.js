ba80a88762a462e9fac9bd70576b5898b18fb1de
Martin Kröning <m.kroening@hotmail.de>
2018-09-24 13:07:25 +0200

    Fulfill PWA criteria https://www.gatsbyjs.org/docs/progressive-web-app/

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  pathPrefix: `/moore-reduction`,
+    pathPrefix: `/moore-reduction`,
 };
+
+plugins: [
+    {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+            name: `moore-reduction`,
+            short_name: `moore`,
+            start_url: `/moore-reduction/`,
+            scope: `/moore-reduction/`,
+            background_color: `#ffffff`,
+            theme_color: `#4352af`,
+            display: `standalone`,
+            icon: `src/images/icon.png`,
+        },
+    },
+    `gatsby-plugin-offline`,
+]
