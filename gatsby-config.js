15a0f5350e180fadd6df4bed5db2f184db3c5b2d
Martin Kröning <m.kroening@hotmail.de>
2018-09-24 13:29:40 +0200

    Fix plugin usage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,19 @@
 module.exports = {
     pathPrefix: `/moore-reduction`,
-};
-
-plugins: [
-    {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-            name: `moore-reduction`,
-            short_name: `moore`,
-            start_url: `/moore-reduction/`,
-            scope: `/moore-reduction/`,
-            background_color: `#ffffff`,
-            theme_color: `#4352af`,
-            display: `standalone`,
-            icon: `src/images/icon.png`,
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `moore-reduction`,
+                short_name: `moore`,
+                start_url: `/moore-reduction/`,
+                scope: `/moore-reduction/`,
+                background_color: `#ffffff`,
+                theme_color: `#4352af`,
+                display: `standalone`,
+                icon: `src/images/icon.png`,
+            },
         },
-    },
-    `gatsby-plugin-offline`,
-]
+        `gatsby-plugin-offline`,
+    ],
+};
